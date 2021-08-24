a21cd31d8f0dc19c0b28bd69773a8c06f36303bb
Adil Salim <1awesomeadil@gmail.com>
2021-08-24 10:34:31 +0200

    changes

--- js-file.js
+++ js-file.js
@@ -1,42 +1,49 @@
 let humanScore = 0;
 let computerScore = 0;
+const buttons = document.querySelectorAll('input');
+
+function restartGame() {
+  alert("Game is restarting");
+  humanScore = 0;
+  computerScore = 0;
+}
+
+const myArray = ["rock", "paper", "scissors"];
+function computerPlay() {
+  return myArray[~~(Math.random() * myArray.length)];
+}
 
 function playRound(playerSelection) {
     let computerSelection = computerPlay();
     let result = "";
 
     if (playerSelection == computerSelection){
-      result = ("Draw. " + playerSelection + " and " + computerSelection + "Are the same");
+      result = ("Draw. " + playerSelection + " and " + computerSelection + " Are the same" + 
+      '<br><br> Your Score: ' + humanScore + " Computer Score: " + computerScore);
     }
     else if ((playerSelection == 'rock' && computerSelection == 'paper')|| 
-    (playerSelection == 'paper' && computerSelection == 'scissor') ||
+    (playerSelection == 'paper' && computerSelection == 'scissors') ||
     (playerSelection == 'rock' && computerSelection == 'paper') ){
         computerScore += 1;
-        result = ("Lose. " + playerSelection + " loses to " + computerSelection);
+        result = ("Lose. " + playerSelection + " loses to " + computerSelection + 
+        '<br><br> Your Score: ' + humanScore + " Computer Score: " + computerScore);
+        if (computerScore == 5) {
+          result += '<br><br> You lost the game!'
+          restartGame()
+        }
     }
-    else if ((playerSelection == 'rock' && computerSelection == 'scissors')|| 
-    (playerSelection == 'paper' && computerSelection == 'rock') ||
-    (playerSelection == 'rock' && computerSelection == 'scissors'))
-     {
+    else{
         humanScore += 1;
-        result = ("Win. " + playerSelection + " beats " + computerSelection);
+        result = ("Win. " + playerSelection + " beats " + computerSelection + 
+        '<br><br> Your Score: ' + humanScore + " Computer Score: " + computerScore);
+        if (humanScore == 5) {
+          result += '<br><br> You won the game!'
+          restartGame()
+        }
     }
-    document.getElementById('result').innerHTML = result;
-  }
-
-  function alertIfIncorrect(){
-      if (playerSelection !== 'rock' || playerSelection !== 'paper' || playerSelection !== 'scissors') {
-          alert("Please choose: Rock, Paper or Scissors");
-      }
-  }
-
-
-const myArray = ["rock", "paper", "scissors"];
-
-function computerPlay() {
-  return myArray[~~(Math.random() * myArray.length)];
+  document.getElementById('result').innerHTML = result;
 }
-const buttons = document.querySelectorAll('input')
+
 buttons.forEach(button => {
   button.addEventListener('click', function(){
     playRound(button.value)
